9e7acf3a0e61ee050f3bc88a947fdb52f79ec74c
= <ryaninternet62@gmail.com>
2025-11-15 09:18:48 -0300

    Limitação para Playwright.

--- primefaces-test/src/test/java/com/playwright/PlaywrightTest.java
+++ primefaces-test/src/test/java/com/playwright/PlaywrightTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,15 +13,18 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+@Disabled
 @DisplayName("Playwright")
 class PlaywrightTest {
+	
+	private String url = "http://localhost:8080/primefaces-test/data.jsf";
 	
 	@Test
 	void chromium() {
 		try (Playwright playwright = Playwright.create()) {
 			Browser browser = playwright.chromium().launch();
 			Page page = browser.newPage();
-			page.navigate("http://localhost:8080/primefaces-test/data.jsf");
+			page.navigate(url);
 			
 			assertEquals("Data", page.title());
 		}
@@ -32,7 +36,7 @@ class PlaywrightTest {
 			Browser browser = playwright.webkit().launch();
 			Page page = browser.newPage();
 			
-			page.navigate("http://localhost:8080/primefaces-test/data.jsf");
+			page.navigate(url);
 			page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
 			
 			assertEquals("Data", page.title());
@@ -45,7 +49,7 @@ class PlaywrightTest {
 			Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
 			Page page = browser.newPage();
 			
-			page.navigate("http://localhost:8080/primefaces-test/data.jsf");
+			page.navigate(url);
 			page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
 			
 			assertEquals("Data", page.title());
