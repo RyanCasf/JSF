50196c2aeaa8c3b653dbe011e8ae420577e0102c
= <ryaninternet62@gmail.com>
2026-02-16 11:55:09 -0300

    Correção para estilização no teste da guia de data

--- primefaces-test/cypress/cypress/e2e/data/data.cy.js
+++ primefaces-test/cypress/cypress/e2e/data/data.cy.js
@@ -16,17 +16,11 @@ function init() {
 
     cy.get('#formularioData\\:texto_data')
         .should('have.text', 'Data:')
-        .should('have.css', 'font-weight', '700')
         .should('have.css', 'display', 'block')
 
     cy.get('#formularioData\\:data')
         .should('be.visible')
 
-    cy.get('#formularioData\\:dataTexto')
-        .should('be.empty')
-        .should('have.css', 'font-weight', '700')
-
     cy.get('#formularioData\\:imprimir')
         .should('have.text', 'Imprimir')
-        .should('have.css', 'display', 'block')
 }
