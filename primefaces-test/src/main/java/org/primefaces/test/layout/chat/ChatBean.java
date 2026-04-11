f6ae70e767caca13ccf8936da07204cc1109c87a
= <ryaninternet62@gmail.com>
2026-04-11 18:41:22 -0300

    Correção para chat

--- primefaces-test/src/main/java/org/primefaces/test/layout/chat/ChatBean.java
+++ primefaces-test/src/main/java/org/primefaces/test/layout/chat/ChatBean.java
@@ -17,19 +17,19 @@ public class ChatBean implements Serializable {
 	private ChatNegocio negocio = new ChatNegocio();
 	
 	public void adicionarInteracao() {
-		
+		System.out.println("adicionarInteracao");
 	}
 	
 	public void selecionarInteracao(int index) {
-		
+		System.out.println("selecionarInteracao");
 	}
 	
 	public void prepararCsv(int index) {
-		
+		System.out.println("prepararCsv");
 	}
 	
 	public void prepararDadosApresentacao(int index) {
-		
+		System.out.println("prepararDadosApresentacao");
 	}
 	
 	public String contatoSelecionado(int index) {
