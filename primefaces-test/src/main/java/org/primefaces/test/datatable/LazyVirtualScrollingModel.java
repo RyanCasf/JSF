eb7251725a76911974ca85f4308eb66c1e7e3817
= <ryaninternet62@gmail.com>
2026-04-29 20:05:35 -0300

    Ajustes para VirtualScrolling

--- primefaces-test/src/main/java/org/primefaces/test/datatable/LazyVirtualScrollingModel.java
+++ primefaces-test/src/main/java/org/primefaces/test/datatable/LazyVirtualScrollingModel.java
@@ -2,27 +2,18 @@ package org.primefaces.test.datatable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 import org.primefaces.model.FilterMeta;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortMeta;
-import org.primefaces.model.SortOrder;
 
 public class LazyVirtualScrollingModel extends LazyDataModel<Entidade> {
 	
 	@Override
 	public List<Entidade> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
-		return load(first, pageSize, null, null, null);
-	}
-	
-	@Override
-	public int count(Map<String, FilterMeta> filterBy) {
-		return 2_000;
-	}
-	
-	public List<Entidade> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String,Object> filters) {
 		System.out.println("first: " + first);
 		System.out.println("pageSize: " + pageSize);
 		System.out.println("calc: " + (first + pageSize));
@@ -31,5 +22,24 @@ public class LazyVirtualScrollingModel extends LazyDataModel<Entidade> {
 		return LongStream.rangeClosed(first, (first + pageSize))
 		        .mapToObj(Entidade::new)
 		        .collect(Collectors.toList());
+	}
+	
+	@Override
+	public int count(Map<String, FilterMeta> filterBy) {
+		return 2_000;
+	}
+	
+	@Override
+	public Entidade getRowData(String rowKey) {
+		if (rowKey == null) return null;
+		return new Entidade(Long.parseLong(rowKey));
+	}
+	
+	@Override
+	public String getRowKey(Entidade object) {
+		if (object == null) return null;
+		return Optional.ofNullable(object.getOrdem())
+				.map(Object::toString)
+				.orElse(null);
 	}
 }
